01fa84d7a996a87b0a391a1b649997c01db0d305
Lanz Joseph Tarrosa Villanueva <120180277+Mooyi07@users.noreply.github.com>
2025-04-28 20:30:40 +0800

    Update Array_Sort.java

--- Array_Sort.java
+++ Array_Sort.java
@@ -1,43 +1,49 @@
-import java.util.Scanner; 
-public class Array_Sort {
-    public static void main(String[] args){
-        Scanner s1 = new Scanner(System.in); // Declares the name of the scanner
-        
-        System.out.print("Enter number of arrays: "); // inputs the maximum number of arrays
-        int input = s1.nextInt();
-        
-        System.out.println("Input " + input + " number/s:"); // inputs the array with certain values
-        int[] arr = new int[input];
-        for (int i = 0; i < input; i++){
-            arr[i] = s1.nextInt();
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter " + size + " number(s):");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
         }
-        
-        System.out.print("Original Array: "); // prints out the original values of the array
-        for (int i = 0; i < input; i++){
-            System.out.print(arr[i]);
-            if (i < input-1){
+
+        System.out.print("Original Array: ");
+        printArray(array);
+
+        sortArrayAscending(array);
+
+        System.out.print("Sorted Array: ");
+        printArray(array);
+
+        scanner.close();
+    }
+
+    private static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
                 System.out.print(", ");
             }
         }
+        System.out.println(); // Move to the next line after printing
+    }
 
-        System.out.println(); // new line
-        
-        for (int i = 0; i < input; i++){
-            for (int j = i + 1; j < input; j++){
-                if(arr[i] > arr[j]){ // The algorithm of ascending sort. If you want descending change ">" to "<"
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+    private static void sortArrayAscending(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    // Swap elements
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
                 }
             }
         }
-        System.out.print("Sorted Array: "); // prints out the sorted array
-        for (int i = 0; i < input; i++){
-            System.out.print(arr[i]);
-             if (i < input-1){
-                System.out.print(", ");
-            }
-        }
-        s1.close(); // closes the scanner
     }
 }
